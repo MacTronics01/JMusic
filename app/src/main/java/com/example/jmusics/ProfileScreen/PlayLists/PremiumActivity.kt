d8ed8bfdc46277b1803c497eb81af4d597317d17
mactronics <ubonmacaulay@gmail.com>
2021-10-06 00:22:00 +0100

    third commit

---
+++ app/src/main/java/com/example/jmusics/ProfileScreen/PlayLists/PremiumActivity.kt
@@ -0,0 +1,15 @@
+package com.example.jmusics.ProfileScreen.PlayLists
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jmusics.R
+
+class PremiumActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_premium)
+
+
+    }
+}
