29e5ef904fc02c2ca5e7afa1b58a6327e18a3ec6
Uticodes <etim.utibe@gmail.com>
2021-10-06 02:00:31 +0100

    created user profile page

---
+++ app/src/main/java/com/example/jmusics/ProfileScreen/PlayLists/Following/Following.kt
@@ -0,0 +1,12 @@
+package com.example.jmusics.ProfileScreen.PlayLists.Following
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jmusics.R
+
+class Following : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_following)
+    }
+}
---
+++ app/src/main/java/com/example/jmusics/ProfileScreen/PlayLists/Following/FollowingUsers.kt
@@ -0,0 +1,4 @@
+package com.example.jmusics.ProfileScreen.PlayLists.Following
+
+class FollowingUsers {
+}
